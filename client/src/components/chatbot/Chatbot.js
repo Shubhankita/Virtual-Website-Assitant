c66b25004baead84bf910c69a0c2ec33d791da9f
Shubhankita <63542702+Shubhankita@users.noreply.github.com>
2021-10-01 14:43:35 +0530

    Masterclass Reply Creation

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -76,8 +76,14 @@ class Chatbot extends Component {
         event.preventDefault();
         event.stopPropagation();
 
-        this.df_text_query(text);
-
+        switch (payload) {
+            case 'training_masterclass':
+                this.df_event_query('MASTERCLASS');
+                break;
+            default:
+                this.df_text_query(text);
+                break;
+        }
     }
 
     renderCards(cards) {
